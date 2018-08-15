3c8344738a2a2227b3f00cc067f0c7e7eb23552d
Thiago Moreira <tmoreira2020@gmail.com>
2018-08-15 17:23:31 -0300

    Add thiago as a regular user

--- init.groovy.d/2-setup-admin-user.groovy
+++ init.groovy.d/2-setup-admin-user.groovy
@@ -16,10 +16,22 @@ def allowsSignup = false
 def hudsonRealm = new HudsonPrivateSecurityRealm(allowsSignup)
 
 def adminUser = hudsonRealm.createAccount("admin", "test")
+def customerUser = hudsonRealm.createAccount("thiago", "test")
 jenkins.setSecurityRealm(hudsonRealm)
 
 def strategy = new GlobalMatrixAuthorizationStrategy()
 strategy.add(Jenkins.ADMINISTER, "admin")
+
+strategy.add(Jenkins.READ, "thiago")
+strategy.add(hudson.model.View.READ, "thiago")
+
+strategy.add(hudson.model.Item.BUILD, "thiago")
+strategy.add(hudson.model.Item.READ, "thiago")
+strategy.add(hudson.model.Item.WIPEOUT, "thiago")
+strategy.add(hudson.model.Item.WORKSPACE, "thiago")
+
+strategy.add(hudson.model.Run.UPDATE, "thiago")
+
 jenkins.setAuthorizationStrategy(strategy)
 
 jenkins.save()
